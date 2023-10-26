818c366edf9720166aa6d41d2a104b1267e2d7b2
vilanovaprojects <mcvmoises@gmail.com>
2023-10-26 22:17:22 +0200

    cap18

--- FineBlog/Areas/Admin/Controllers/PostController.cs
+++ FineBlog/Areas/Admin/Controllers/PostController.cs
@@ -1,6 +1,13 @@
-﻿using FineBlog.ViewModels;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using FineBlog.Data;
+using FineBlog.Models;
+using FineBlog.Utilites;
+using FineBlog.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FineBlog.Areas.Admin.Controllers
 {
@@ -8,15 +15,98 @@ namespace FineBlog.Areas.Admin.Controllers
     [Authorize]
     public class PostController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public PostController(ApplicationDbContext context, INotyfService notyfService, IWebHostEnvironment webHostEnvironment,UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _context = context;
+            _notification = notyfService;
+            _webHostEnvironment = webHostEnvironment;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int? page)
+        {
+            var listOfPosts = new List<Post>();
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+            if (loggedInUserRole[0] == WebsiteRoles.WebsiteAdmin)
+            {
+                listOfPosts = await _context.Posts!.Include(x => x.ApplicationUser).ToListAsync();
+            }
+            else
+            {
+                listOfPosts = await _context.Posts!.Include(x => x.ApplicationUser).Where(x => x.ApplicationUser!.Id == loggedInUser!.Id).ToListAsync();
+            }
+
+            var listOfPostsVM = listOfPosts.Select(x => new PostVM()
+            {
+                Id = x.Id,
+                Title = x.Title,
+                CreatedDate = x.CreatedDate,
+                ThumbnailUrl = x.ThumbnailUrl,
+                AuthorName = x.ApplicationUser!.FirstName + " " + x.ApplicationUser.LastName
+            }).ToList();
+
+
+            return View(listOfPostsVM);
         }
 
         [HttpGet]
         public IActionResult Create()
         {
             return View(new CreatePostVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePostVM vm)
+        {
+            if (!ModelState.IsValid) { return View(vm); }
+
+            //get logged in user id
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+
+            var post = new Post();
+
+            post.Title = vm.Title;
+            post.Description = vm.Description;
+            post.ShortDescription = vm.ShortDescription;
+            post.ApplicationUserId = loggedInUser!.Id;
+
+            if (post.Title != null)
+            {
+                string slug = vm.Title!.Trim();
+                slug = slug.Replace(" ", "-");
+                post.Slug = slug + "-" + Guid.NewGuid();
+            }
+
+            if (vm.Thumbnail != null)
+            {
+                post.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+
+            await _context.Posts!.AddAsync(post);
+            await _context.SaveChangesAsync();
+            _notification.Success("Post Created Successfully");
+            return RedirectToAction("Index");
+        }
+
+
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filePath = Path.Combine(folderPath, uniqueFileName);
+            using (FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+            return uniqueFileName;
         }
     }
 }
--- FineBlog/Models/Post.cs
+++ FineBlog/Models/Post.cs
@@ -10,7 +10,7 @@
         public ApplicationUser? ApplicationUser { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public string? Description { get; set; }
-        public string? slug { get; set; }
+        public string? Slug { get; set; }
         public string? ThumbnailUrl { get; set; }
     }
 }
